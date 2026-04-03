379e3a5da368c77ef5b76ab6106cef01d5df3467
shalinigowda578-star <shalinigowda578@gmail.com>
2026-04-04 00:40:12 +0530

    Add files via upload

---
+++ Task2_SpamDetection/spam_detection.py
@@ -0,0 +1,43 @@
+
+# Spam SMS Detection
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv("spam.csv", encoding='latin-1')
+
+# Keep needed columns
+df = df[['v1', 'v2']]
+df.columns = ['label', 'message']
+
+# Convert labels
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    df['message'], df['label'], test_size=0.2, random_state=42)
+
+# Convert text → numbers
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+# Train model
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Accuracy
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+# Test custom message
+sample = ["Congratulations! You won a free ticket"]
+sample_vec = vectorizer.transform(sample)
+print("Prediction:", model.predict(sample_vec))
